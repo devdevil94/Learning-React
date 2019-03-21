d31d652372a370cf1484c697a2480fde53554be6
Saud Al Alawi <41317640+devdevil94@users.noreply.github.com>
2019-03-21 22:19:11 +0400

    introduction

---
+++ src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function MyInfo() {
+  return (
+    <div className="info">
+      <h1>Saud Lawi</h1>
+      <p>Self-taught web developer</p>
+      <ul>
+        <li>Italy</li>
+        <li>Spain</li>
+        <li>Malta</li>
+      </ul>
+    </div>
+  );
+}
+
+export default MyInfo;
