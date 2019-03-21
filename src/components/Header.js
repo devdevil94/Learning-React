e22f9993a8c846978d1f2bbcd1a3c8e725b2b3e9
Saud Al Alawi <41317640+devdevil94@users.noreply.github.com>
2019-03-21 22:40:52 +0400

    Nested Components

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <a href="#">
+        <div>Logo</div>
+      </a>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Blog</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import MyInfo from "./components/MyInfo.js";
+import Footer from "./components/Footer.js";
+import Header from "./components/Header.js";
 
 import "./styles.css";
 
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MyInfo />
+      <Footer />
+    </div>
+  );
+}
+
 const rootElement = document.getElementById("root");
-ReactDOM.render(<MyInfo />, rootElement);
+ReactDOM.render(<App />, rootElement);
